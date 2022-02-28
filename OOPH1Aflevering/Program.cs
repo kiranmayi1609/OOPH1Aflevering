96ed97bc4652bf5d3ac266a2ed7bc0604c6f4a0a
kiranmayi <kiranmayi1609@gmail.com>
2022-02-28 09:27:49 +0100

    Add project files.

---
+++ OOPH1Aflevering/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear ();
+Console.WriteLine("Vælge en uddannelselinje");
+Console.WriteLine("1.Del 1");
+Console.WriteLine("2.Del 2 :Project");
+Console.WriteLine("3.Exit");
+Console.WriteLine("\r\n Vælge en option :");
+
+
+
